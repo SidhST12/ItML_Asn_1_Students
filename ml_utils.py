fa2db468a1f4acbe33fa3f13633b4341769fbde1
sthapliyal1@nait.ca <sthapliyal1@nait.ca>
2023-01-22 13:38:06 -0700

    update

--- ml_utils.py
+++ ml_utils.py
@@ -125,7 +125,7 @@ class edaDF:
             figure.show()
         return figure
 
-    def des(self):
+    def stats(self):
         return self.data.describe()
 
     def fullEDA(self):
@@ -154,4 +154,4 @@ class edaDF:
             plt.show(fig3)
 
         with out4:
-            self.des()
+            self.stats()
